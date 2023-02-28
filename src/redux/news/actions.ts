6d1be05e5784473e5f714171d7c0caf0f937100a
alexlogvinmal <alexlogvinmal@gmail.com>
2023-03-01 00:02:34 +0200

    added comments

--- src/redux/news/actions.ts
+++ src/redux/news/actions.ts
@@ -11,20 +11,20 @@ export const fetchPostsSuccess = (data: any) => ({
   payload: data,
 });
 
-export const fetchPostsFailure = (error: string) => ({
+export const fetchPostsFailed = (error: string) => ({
   type: PostActionTypes.FETCH_POSTS_FAILED,
   payload: error,
 });
 
 export const fetchPosts = () => {
   return async (dispatch: Dispatch) => {
-    dispatch(fetchPostsRequest());
+    dispatch(fetchPostsRequest()); // Визиваємо action завантаження 
     try {
       const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
       const data = await response.data;
-      dispatch(fetchPostsSuccess(data));
+      dispatch(fetchPostsSuccess(data)); // Визиваємо action коли всі дані отримані
     } catch (error:any) {
-      dispatch(fetchPostsFailure(error.message));
+      dispatch(fetchPostsFailed(error.message)); // Визиваємо action якщо виникла помилка 
     }
   };
 };
