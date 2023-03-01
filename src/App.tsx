81b2d7a0e84c5172b4cf3c05a43b502cab33c91d
alexlogvinmal <alexlogvinmal@gmail.com>
2023-03-02 00:00:28 +0200

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Main  from './components/Main';
-import News  from "./components/News";
+import Main from './components/Main';
+import News from "./components/News";
 import Login from './components/Login';
-import Profile  from './components/Profile';
+import Profile from './components/Profile';
 import Footer from './components/Footer';
 import NotFound from './components/NotFound';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
 
-
 function App() {
-
 
   return (
     <>
       <Header />
-      <Container fixed sx={{backgroundColor:'white', boxShadow:'0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)' }}>
-        <Box sx={{ minHeight: '40rem', textAlign:'center', paddingTop:'1rem' }}>
+      <Container fixed sx={{ backgroundColor: 'white', flexGrow: '1', boxShadow: '0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)' }}>
+        <Box sx={{ minHeight: '40rem', textAlign: 'center', paddingTop: '1rem' }}>
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/news" element={<News />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/login" element={<Login />} />
-            <Route path="*" element={<NotFound/>} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Box>
       </Container>
-      <Footer/>
+      <Box sx={{ flexShrink: '0' }}>
+        <Footer />
+      </Box>
     </>
   )
 };
